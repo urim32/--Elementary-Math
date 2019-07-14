06ad10871521a1d662a8c04bb7d8161e99654273
uri malasa <urimalasa30@gmail.com>
2019-07-14 08:42:26 +0300

    added get rndom question function

--- elementary_math.py
+++ elementary_math.py
@@ -5,31 +5,40 @@ questions = 0
 wrong_ans = []
 
 
-def elementary_math():
-    a = random.randint(1, 30)
-    b = random.randint(1, 30)
-    global correct_ans
-    global questions
-    global wrong_ans
-    questions += 1
+def get_rnd_question():
+    a = random.randint(1, 10)
+    b = random.randint(1, 10)
     signs = {"+": operator.add, "-": operator.sub,
              "*": operator.mul}
     rnd_opr = random.choice(list(signs.keys()))
     art_qs = f"{a} {rnd_opr} {b} = "
     print(art_qs + "?")
     ans = signs[rnd_opr](a, b)
+    return ans, art_qs
+
+
+def math_quiz():
+
+    global correct_ans
+    global questions
+    global wrong_ans
+    questions += 1
+    ans_art_qs_tupple = get_rnd_question()
+    ans = ans_art_qs_tupple[0]
+    art_qs = ans_art_qs_tupple[1]
     user_ans = int(input("enter your enswer: "))
     yes_list = ["yes", "Yes", "y", "Y"]
     no_list = ["no", "No", "n", "N", ""]
 
-    art_qs = f"{a} {rnd_opr} {b} = {ans} ({user_ans}) "
+    art_qs = art_qs + f"{str(ans)} ({str(user_ans)})"
     if user_ans == ans:
         correct_ans += 1
         print("correct answer")
         user_selct = input("do you like play again? :")
         if user_selct in yes_list:
-            elementary_math()
+            math_quiz()
         if user_selct in no_list:
+            print("this are all your incorrect questions:")
             for x in wrong_ans:
                 print(x)
     else:
@@ -37,13 +46,14 @@ def elementary_math():
         print("wrong answer")
         user_selct = input("do you like play again? :")
         if user_selct in yes_list:
-            elementary_math()
+            math_quiz()
         if user_selct in no_list:
+            print("this are all your incorrect questions:")
             for x in wrong_ans:
                 print(x)
     return f"You have answered correctly {correct_ans} out of {questions} problems"
 
 
-x = elementary_math()
+x = math_quiz()
 
 print(x)
